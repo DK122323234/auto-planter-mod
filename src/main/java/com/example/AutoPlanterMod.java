56363e49190d53acc3e6b28210a3a9a3e9ed6e10
DK122323234 <dkamensky@licei10.ru>
2026-01-10 00:22:16 +0300

    подготовил среду разработки

---
+++ src/main/java/com/example/AutoPlanterMod.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AutoPlanterMod implements ModInitializer {
+    public static final String MODID = "auto_plant";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
+
+    @Override
+    public void onInitialize() {
+        // This code runs as soon as Minecraft is in a mod-load-ready state.
+        // However, some things (like resources) may still be uninitialized.
+        // Proceed with mild caution.
+
+        LOGGER.info("Hello Fabric world!");
+    }
+}
